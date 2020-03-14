925af172e8a6a4b18b29d135415c0e09065ec918
ericksen-github <ericksen.github@gmail.com>
2020-03-14 01:27:51 +0100

    Updates

---
+++ precache-manifest.41e32bde1cdd00601a3d587626747a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb30264a4c54f2affbd85e06752915d6",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "a2883d7a09bfc863765b",
+    "url": "/weather_app/static/css/main.d52e282a.chunk.css"
+  },
+  {
+    "revision": "56c070e476eea22dea78",
+    "url": "/weather_app/static/js/2.e7b2c7e1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather_app/static/js/2.e7b2c7e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2883d7a09bfc863765b",
+    "url": "/weather_app/static/js/main.3c587c5e.chunk.js"
+  },
+  {
+    "revision": "591e2505f6ee8d973312",
+    "url": "/weather_app/static/js/runtime-main.207e6037.js"
+  }
+]);
